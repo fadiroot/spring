4964b66553fc8af9b25b99cb10f0da809f110891
fedi.romdhan <fadiromdhan2@gmail.com>
2024-12-06 16:52:42 +0100

    di explaine

---
+++ src/main/java/com/softy/shop/Application.java
@@ -0,0 +1,19 @@
+package com.softy.shop;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+	}
+
+}
+ 
